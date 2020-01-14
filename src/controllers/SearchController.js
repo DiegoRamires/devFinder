be7f9c59546a197c35cb5553f2daa7b846be7b2e
DiegoRamires <goncalves.ramires@gmail.com>
2020-01-14 17:01:02 -0300

    improve dev search adding coordinate filter

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -11,6 +11,15 @@ export default {
       techs: {
         $in: techsArray,
       },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: 10000,
+        },
+      },
     });
     return res.json({ devs });
   },
