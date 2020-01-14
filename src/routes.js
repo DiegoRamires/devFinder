5ae22386dac90f8cbf46e8b8e654d0ed122b84d4
DiegoRamires <goncalves.ramires@gmail.com>
2020-01-14 15:01:31 -0300

    save user data on database

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,31 @@
 import { Router } from 'express';
 
+import Dev from './models/Dev';
+
 import API from './utils/apiUrl';
 
 const routes = Router();
 
 routes.post('/devs', async (req, res) => {
-  const { github_username } = req.body;
+  const { github_username, techs } = req.body;
 
   const API_response = await API.get(`${github_username}`);
-  console.log(API_response.data);
 
-  return res.json({ message: 'Hello world' });
+  const { name = login, avatar_url, bio } = API_response.data;
+
+  console.log(name, avatar_url, bio, github_username);
+
+  const techsArray = techs.split(',').map(tech => tech.trim());
+
+  const dev = await Dev.create({
+    github_username,
+    name,
+    avatar_url,
+    bio,
+    techs: techsArray,
+  });
+
+  return res.json(dev);
 });
 
 export default routes;
