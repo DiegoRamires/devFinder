a50d70e10047a9b1eda58c5cad2615f398eac868
DiegoRamires <goncalves.ramires@gmail.com>
2020-01-14 13:45:33 -0300

    create editorconfig, install and configure eslint and prettier to reinforce code styling. configure sucrase import and export files

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-const express = require("express");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-require("dotenv/config");
+import express from 'express';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+
+import 'dotenv/config';
 
 const app = express();
 mongoose.connect(`${process.env.MongoDB}`, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
-app.post("/", (req, res) => {
+app.post('/', (req, res) => {
   console.log(req.body);
-  return res.json({ message: "Hello world" });
+  return res.json({ message: 'Hello world' });
 });
 
 const port = process.env.PORT || 3333;
-app.listen(port, process.env.IP, function() {
-  console.log("The App server is running!");
+app.listen(port, process.env.IP, () => {
+  console.log('The App server is running!');
   console.log(`Access http://localhost/${port}`);
-  console.log("Hit ctrl + c to stop!");
+  console.log('Hit ctrl + c to stop!');
 });
