56e9d9fa40f9822b7c33d2c9f48f87d09d0234da
DiegoRamires <goncalves.ramires@gmail.com>
2020-01-17 15:44:10 -0300

    install socket.io

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import http from 'http';
 import 'dotenv/config';
 
 import routes from './routes';
@@ -13,12 +14,14 @@ mongoose.connect(`${process.env.MongoDB}`, {
   useFindAndModify: false,
 });
 
+const server = http.Server(app);
+
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3333, process.env.IP, () => {
+server.listen(process.env.PORT || 3333, process.env.IP, () => {
   console.log('The App server is running!');
   console.log(`Access http://localhost/${process.env.PORT}`);
   console.log('Hit ctrl + c to stop!');
